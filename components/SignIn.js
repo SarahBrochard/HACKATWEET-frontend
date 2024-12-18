5820ce69f1dfd79c8161166f822861baa451a42d
Willy-Thai <haruki.thai@gmail.com>
2024-12-18 21:38:44 +0100

    redirection vers page home une fois connecté ok

--- components/SignIn.js
+++ components/SignIn.js
@@ -3,20 +3,22 @@ import Link from "next/link";
 import styles from '../styles/SignIn.module.css';
 import Image from 'next/image'
 
-import Home from "./Home";
+import { Button, Modal } from 'antd';
 
 import { useDispatch, useSelector } from 'react-redux';
 import { login, logout } from '../reducers/user';
 
+import {useRouter} from "next/router";
 
 
 function SignIn() {
     const [signInUsername, setSignInUsername] = useState("");
     const [signInPassword, setSignInPassword] = useState("");
-    const [redirect, setRedirect] = useState(false);
+    // const [redirect, setRedirect] = useState(false);
 
     const dispatch = useDispatch();
 	const user = useSelector((state) => state.user.value);
+    const router = useRouter();
     
 
     const auClickSurSignIn = () => {
@@ -30,24 +32,19 @@ function SignIn() {
                 if(data.result) {
                     dispatch(login({ username: signInUsername, token: data.token }));
                     setRedirect(true);
-                    <Home/>
+                    router.push("/home")
                 }
             });
     };
-
-    // if(redirect) {
-    //     return(<Link href="/home"> Go to Home </Link>)
-    // }
-
-
 
     return (
         <div className={styles.divSignIn}>
             <Image src="/twitter.png" alt="logo" width="60px" height="60px" className={styles.logoTwitter}/>
             <input onChange={(e) => setSignInUsername(e.target.value)} value={signInUsername} placeholder='Username'/>
             <input onChange={(e) => setSignInPassword(e.target.value)} value={signInPassword} placeholder='Password'/>
-            <button onClick={()=> auClickSurSignIn()}>SignIn</button>
-            {redirect && <Link href="/home"> Go to Home </Link>}
+            {/* <button onClick={()=> auClickSurSignIn()}>SignIn</button> */}
+            <Button onClick={()=> auClickSurSignIn()}>SignIn</Button>
+            {/* {redirect && <Link href="/home"> Go to Home </Link>} */}
         </div>
     )
 
--- components/SignUp.js
+++ components/SignUp.js
@@ -3,22 +3,23 @@ import Image from 'next/image'
 import styles from '../styles/SignUp.module.css';
 import Link from "next/link";
 import Home from "./Home";
+import { Button, Modal } from 'antd';
     
 import { useDispatch, useSelector } from 'react-redux';
 import { login, logout } from '../reducers/user';
 
-
+import {useRouter} from "next/router";
 
 function SignUp() {
 
     const [signUpFirstname, setSignUpFirstname] = useState("");
     const [signUpUsername, setSignUpUsername] = useState("");
     const [signUpPassword, setSignUpPassword] = useState("");
-    const [redirect, setRedirect] = useState(false);
+    // const [redirect, setRedirect] = useState(false);
 
     const dispatch = useDispatch();
 	const user = useSelector((state) => state.user.value);
-
+    const router = useRouter();
     
 
     const auClickSurSignUp = () => {
@@ -31,8 +32,8 @@ function SignUp() {
                 console.log(data)
                 if(data.result) {
                     dispatch(login({ username: signUpUsername, token: data.token }));
-                    setRedirect(true);
-                    return(<Home/>)
+                    // setRedirect(true);
+                    router.push("/home")
                 }
             });
     };
@@ -42,8 +43,9 @@ function SignUp() {
             <input onChange={(e) => setSignUpFirstname(e.target.value)} value={signUpFirstname} placeholder='Firstname'/>
             <input onChange={(e) => setSignUpUsername(e.target.value)} value={signUpUsername} placeholder='Username'/>
             <input onChange={(e) => setSignUpPassword(e.target.value)} value={signUpPassword} placeholder='Password'/>
-            <button onClick={()=> auClickSurSignUp()}>SignUp</button>
-            {redirect && <Link href="/home"> Go to Home </Link>}
+            {/* <button onClick={()=> auClickSurSignUp()}>SignUp</button> */}
+            <Button onClick={()=> auClickSurSignUp()}>SignUp</Button>
+            {/* {redirect && <Link href="/home"> Go to Home </Link>} */}
         </div>
     )
 
