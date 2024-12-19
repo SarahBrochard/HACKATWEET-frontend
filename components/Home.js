8a91c087ff02c988fb69a3e6d589c640a6b74e6f
Deb <d.setboune@live.fr>
2024-12-19 18:55:35 +0100

    ds19.12.24

--- components/Home.js
+++ components/Home.js
@@ -1,31 +1,27 @@
-import styles from '../styles/Home.module.css';
-import { useEffect, useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import Head from 'next/head';
-import LastTweets from './LastTweets';
-import Tweet from './Tweet';
-import Trend from './Trend';
-
+import styles from "../styles/Home.module.css";
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import Head from "next/head";
+import LastTweets from "./LastTweets";
+import Tweet from "./Tweet";
+import Trend from "./Trend";
 
 function Home() {
 
 
-
-
-
-
+  
   return (
     <div>
       <main className={styles.main}>
         <h1 className={styles.title}> HOME </h1>
-         
-         <Tweet/>
-         <button id='logout' className={styles.logBt}>Logout</button>
-      </main>
-      <div>
-        
+        <Tweet />
+        <div className={styles.menuleft}>
+        <button id="logout" className={styles.logBt}>
+          Logout
+        </button>
         </div>
-      
+      </main>
+      <div></div>
     </div>
   );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import '../styles/globals.css';
 import Head from 'next/head';
 import user from '../reducers/user'
@@ -7,7 +6,7 @@ import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
 
 const store = configureStore({
- reducer: {user },
+ reducer: { user },
 });
 
 function App({ Component, pageProps }) {
@@ -26,68 +25,39 @@ export default App;
 
 
 
-=======
->>>>>>> 1692855823db5cd7fa0802c9735522966451f234
 // import '../styles/globals.css';
 // import Head from 'next/head';
-// import user from '../reducers/user'
 
 // import { Provider } from 'react-redux';
-// import { configureStore } from '@reduxjs/toolkit';
+// import user from '../reducers/user';
+// import { persistStore, persistReducer } from 'redux-persist';
+// import { PersistGate } from 'redux-persist/integration/react';
+// import storage from 'redux-persist/lib/storage';
+// import { combineReducers, configureStore } from '@reduxjs/toolkit';
+
+
+// const reducers = combineReducers({ user });
+   
+// const persistConfig = { key: 'hackatweet', storage };
 
 // const store = configureStore({
-//  reducer: { user },
-// });
+//     reducer: persistReducer(persistConfig, reducers),
+//     middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }),
+//    });
+
+// const persistor = persistStore(store);
 
 // function App({ Component, pageProps }) {
-//  return (
-//    <Provider store={store}>
-//      <Head>
-//        <title>HACKATWEET</title>
-//      </Head>
-//      <Component {...pageProps} />
-//    </Provider>
-//  );
-// }
-
-// export default App;
-
-
-
-
-import '../styles/globals.css';
-import Head from 'next/head';
-
-import { Provider } from 'react-redux';
-import user from '../reducers/user';
-import { persistStore, persistReducer } from 'redux-persist';
-import { PersistGate } from 'redux-persist/integration/react';
-import storage from 'redux-persist/lib/storage';
-import { combineReducers, configureStore } from '@reduxjs/toolkit';
-
-
-const reducers = combineReducers({ user });
-   
-const persistConfig = { key: 'hackatweet', storage };
-
-const store = configureStore({
-    reducer: persistReducer(persistConfig, reducers),
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }),
-   });
-
-const persistor = persistStore(store);
-
-function App({ Component, pageProps }) {
-    return (
-      <Provider store={store}>
-        <PersistGate persistor={persistor}>
-        <Head>
-          <title>Hackatweet</title>
-        </Head>
-        <Component {...pageProps} />
-        </PersistGate>
-      </Provider>
-    );
-  }
+//     return (
+//       <Provider store={store}>
+//         <PersistGate persistor={persistor}>
+//         <Head>
+//           <title>Hackatweet</title>
+//         </Head>
+//         <Component {...pageProps} />
+//         </PersistGate>
+//       </Provider>
+//     );
+//   }
   
-  export default App;
+//   export default App;
